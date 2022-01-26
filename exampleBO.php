2283baaa6f00abd0e8b1be17c57a7f01a6a04810
Zaqueu Alves De Oliveira Júnior <69696525+zaqueu-git@users.noreply.github.com>
2022-01-25 23:47:12 -0300

    Add files via upload

---
+++ exampleBO.php
@@ -0,0 +1,23 @@
+<?php
+class ExampleBO
+{
+    private $id;
+    private $name;
+    private $surname;
+    private $address;
+
+    public function __construct()
+    {
+
+    }
+
+    public function getFullName()
+    {
+
+    }
+
+    public function getAddress()
+    {
+
+    }
+}
